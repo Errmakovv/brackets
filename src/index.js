eca60230b662936f1d91a137fb9cdfcd1f040586
Errmakovv <darkday3@mail.ru>
2019-09-23 23:34:58 +0300

    ver 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let masopened=[];
+ let k=0;
+ for(i=0;i<str.length;i++){
+  if((str[i]=='|' || str[i]=='7' || str[i]=='8') && masopened[masopened.length-1]!=str[i] && k==1) return false;
+  else 
+  if((str[i]=='|' || str[i]=='7' || str[i]=='8') && k==0){
+    masopened.push(str[i]);
+    k++;
+  }
+  else 
+  if((str[i]=='|' || str[i]=='7' || str[i]=='8') && masopened[masopened.length-1]==str[i]){
+    masopened.pop();
+    k--;
+  }
+  else
+  for(j=0;j<bracketsConfig.length;j++){
+   
+    if(str[i]==bracketsConfig[j][0]){
+      masopened.push(str[i]);
+      break;
+    }
+    else
+    if(str[i]==bracketsConfig[j][1]){
+      if(masopened.pop()==bracketsConfig[j][0]) break;
+      else return false;
+    }
+  }
+ }
+ if(masopened.length!=0) return false;
+ return true;
 }
