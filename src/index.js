db944d19665988d5f90773a4f4577216542713e6
Timur Yermakou <darkday3@mail.ru>
2019-09-23 23:58:37 +0300

    add resolve function

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ module.exports = function check(str, bracketsConfig) {
  let masopened=[];
  let k=0;
  for(i=0;i<str.length;i++){
-  if((str[i]=='|' || str[i]=='7' || str[i]=='8') && masopened[masopened.length-1]!=str[i] && k==1) return false;
-  else 
-  if((str[i]=='|' || str[i]=='7' || str[i]=='8') && k==0){
+  if((str[i]=='|' || str[i]=='7' || str[i]=='8') && masopened[masopened.length-1]!=str[i]) {
     masopened.push(str[i]);
-    k++;
   }
   else 
   if((str[i]=='|' || str[i]=='7' || str[i]=='8') && masopened[masopened.length-1]==str[i]){
     masopened.pop();
-    k--;
   }
   else
   for(j=0;j<bracketsConfig.length;j++){
